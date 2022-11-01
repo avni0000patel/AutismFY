0948debf49a8f3281fedaa82769b580aa1d8422e
meriem0319 <meriem0319@gmail.com>
2022-11-01 18:35:00 -0400

    typo in connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/autiamfydb', {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/autismfydb",
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});
+  }
+);
 
 module.exports = mongoose.connection;
