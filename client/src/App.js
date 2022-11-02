3ff849fbe944722f4889d1f3ed858e7f12706bb6
Avni <avni0000patel@gmail.com>
2022-11-01 22:46:36 -0400

    login/signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,57 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+// import Home from './components/Home';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import './App.css';
-import Post from './Post';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [posts, setPosts] = useState([
-    {
-      username: "avni0000patel1",
-      imageURL: "https://freepngimg.com/thumb/mario/20698-7-mario-transparent-background.png",
-      caption: "Wow it works!",
-    },
-    {
-      username: "avni0000patel2",
-      imageURL: "https://freepngimg.com/thumb/mario/20698-7-mario-transparent-background.png",
-      caption: "Wow it works!!",
-    },
-  ]);
   return (
-    <div className="App">
-      <div class="app__header">
-        <h1>AutismFY</h1>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Router>
+          <>
+            <Navbar />
+            <Routes>
+              {/* <Route
+                path='/'
+                element={<Home />}
+              /> */}
+              <Route
+                path='*'
+                element={<h1 className='display-2'>Wrong page!</h1>}
+              />
+            </Routes>
+          </>
+        </Router>
       </div>
-      <div className="timeline">
-        {
-          posts.map((post) => (
-            <Post
-              username={post.username}
-              caption={post.caption}
-              imageURL={post.imageURL}
-            />
-          ))
-        }
-      </div>
-    </div>
+    </ApolloProvider >
   );
 }
 
