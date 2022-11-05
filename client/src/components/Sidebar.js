5fb8a515466dcbd8cbdea7583dd709eb84a6c893
meriem0319 <meriem0319@gmail.com>
2022-11-05 12:20:42 -0400

    created messages page, form, and sidebar files, and css for style

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  const contactList = ["1st List", "2nd List", "3rd List"];
+  return (
+    <>
+      <h2>Available Contacts</h2>
+      <ListGroup>
+        {contactList.map((contact, idx) => (
+          <ListGroup.Item key={idx}>{contact}</ListGroup.Item>
+        ))}
+      </ListGroup>
+      <h2>Members</h2>
+    </>
+  );
 }
 
 export default Sidebar;
