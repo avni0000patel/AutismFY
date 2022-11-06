d49d14dc4dd477f5956c51a4124f9f62c0a7e6a9
Avni <avni0000patel@gmail.com>
2022-11-06 16:11:44 -0500

    add avatar to post in home page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -5,6 +5,7 @@ export const QUERY_PROFILES = gql`
     profiles {
       _id
       name
+      avatar
       image
       captions
     }
@@ -16,6 +17,7 @@ export const QUERY_SINGLE_PROFILE = gql`
     profile(profileId: $profileId) {
       _id
       name
+      avatar
       image
       captions
     }
