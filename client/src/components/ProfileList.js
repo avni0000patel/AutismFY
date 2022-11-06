a5118f7b231987498b90d14d96edf377860bc342
Avni <avni0000patel@gmail.com>
2022-11-06 18:29:07 -0500

    remove title

--- client/src/components/ProfileList.js
+++ client/src/components/ProfileList.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Avatar from '@mui/material/avatar';
 
-const ProfileList = ({ profiles, title }) => {
+const ProfileList = ({ profiles }) => {
     const styles = {
         post: {
             backgroundColor: 'white',
             border: '1px solid lightgray',
             maxWidth: '700px',
             marginBottom: '45px',
+            display: 'flex',
+            justifyContent: 'center',
+
         },
         post__header: {
             display: 'flex',
@@ -36,23 +39,19 @@ const ProfileList = ({ profiles, title }) => {
 
     return (
         <div>
-            <h3 className="text-primary">{title}</h3>
-            <div className="flex-row justify-space-between my-4">
-                {profiles &&
-                    profiles.map((profile) => (
-                        <div key={profile._id} className="col-12">
-                            <div className="post" style={styles.post}>
-                                <div className="post__header" style={styles.post__header}>
-                                    <Avatar className="post__avatar" style={styles.post__avatar} alt={profile.name} src={profile.avatar}></Avatar>
-                                    <h3>{profile.name}</h3>
-                                </div>
-                                <img className="post__image" style={styles.post__image} src={profile.image} alt="postimage"></img>
-                                <div className="post__caption" style={styles.post__caption}><strong>{profile.name} </strong>{profile.captions}</div>
-                            </div >
+            {profiles &&
+                profiles.map((profile) => (
+                    <div key={profile._id} className="post card col-12" style={styles.post}>
+                        <div className="post__header card-title" style={styles.post__header}>
+                            <Avatar className="post__avatar" style={styles.post__avatar} alt={profile.name} src={profile.avatar}></Avatar>
+                            <h3>{profile.name}</h3>
                         </div>
-                    ))}
-            </div>
-        </div>
+                        <img className="post__image card-text" style={styles.post__image} src={profile.image} alt="postimage"></img>
+                        <div className="post__caption" style={styles.post__caption}><strong>{profile.name} </strong>{profile.captions}</div>
+                    </div >
+                ))
+            }
+        </div >
     );
 };
 
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -19,7 +19,6 @@ const Home = () => {
                     ) : (
                         <ProfileList
                             profiles={profiles}
-                            title="Your feed"
                         />
                     )}
                 </div>
