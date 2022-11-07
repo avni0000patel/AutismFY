2f157e87596433f64037de54237eb4c80f68376a
Avni <avni0000patel@gmail.com>
2022-11-06 20:35:37 -0500

    update home location

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 // import Search from './pages/Search';
 import Create from "./pages/Create";
 import Messages from "./pages/Messages.js";
@@ -10,43 +10,43 @@ import Messages from "./pages/Messages.js";
 import ProfileContainer from "./pages/Profile/ProfileContainer";
 
 export default function PortfolioContainer() {
-  const styles = {
-    all: {
-      backgroundColor: "lightgray",
-      color: "#ffffff",
-      minHeight: "100vh",
-      fontFamily: "Playfair Display",
-    },
-  };
-  const [currentPage, setCurrentPage] = useState("Home");
+    const styles = {
+        all: {
+            backgroundColor: "lightgray",
+            color: "#ffffff",
+            minHeight: "100vh",
+            fontFamily: "Playfair Display",
+        },
+    };
+    const [currentPage, setCurrentPage] = useState("Home");
 
-  const renderPage = () => {
-    if (currentPage === "Home") {
-      return <Home />;
-    }
-    // if (currentPage === 'Search') {
-    //     return <Search />;
-    // }
-    if (currentPage === "Create") {
-      return <Create />;
-    }
-    if (currentPage === "Messages") {
-      return <Messages />;
-    }
-    if (currentPage === "Profile") {
-      return <ProfileContainer />;
-    }
-    // return <Profile />;
-  };
+    const renderPage = () => {
+        if (currentPage === "Home") {
+            return <Home />;
+        }
+        // if (currentPage === 'Search') {
+        //     return <Search />;
+        // }
+        if (currentPage === "Create") {
+            return <Create />;
+        }
+        if (currentPage === "Messages") {
+            return <Messages />;
+        }
+        if (currentPage === "Profile") {
+            return <ProfileContainer />;
+        }
+        // return <Profile />;
+    };
 
 
-  const handlePageChange = (page) => setCurrentPage(page);
+    const handlePageChange = (page) => setCurrentPage(page);
 
-  return (
-    <div style={styles.all}>
-      <Navbar />
-      {renderPage()}
-      <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
-    </div>
-  );
+    return (
+        <div style={styles.all}>
+            <Navbar />
+            {renderPage()}
+            <Footer currentPage={currentPage} handlePageChange={handlePageChange} />
+        </div>
+    );
 }
