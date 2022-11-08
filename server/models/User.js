6bc03081298e9d4386edad1fd2190f1ee604c317
meriem0319 <meriem0319@gmail.com>
2022-11-07 19:41:34 -0500

    tried using validation for email, didn't work

--- server/models/User.js
+++ server/models/User.js
@@ -1,24 +1,24 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        trime: true,
-        minlength: 3
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/, 'Must use a valid email address'],
-    },
-    password: {
-        type: String,
-        required: true,
-        trime: true,
-        minlength: 3
-    }
+  username: {
+    type: String,
+    required: true,
+    trime: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    trime: true,
+    minlength: 3,
+  },
 });
 const User = mongoose.model("User", userSchema);
 module.exports = User;
