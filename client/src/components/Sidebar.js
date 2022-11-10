1361b2e638c94171904f35af862c564a1a2052ba
meriem0319 <meriem0319@gmail.com>
2022-11-10 15:57:01 -0500

    new note form created

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { ListGroup } from "react-bootstrap";
+// import React from "react";
+// import { ListGroup } from "react-bootstrap";
 
-function Sidebar() {
-  const contactList = ["1st List", "2nd List", "3rd List"];
-  return (
-    <>
-      <h2>Available Contacts</h2>
-      <ListGroup>
-        {contactList.map((contact, idx) => (
-          <ListGroup.Item key={idx}>{contact}</ListGroup.Item>
-        ))}
-      </ListGroup>
-      <h2>Members</h2>
-    </>
-  );
-}
+// function Sidebar() {
+//   const contactList = ["1st List", "2nd List", "3rd List"];
+//   return (
+//     <>
+//       <h2>Available Contacts</h2>
+//       <ListGroup>
+//         {contactList.map((contact, idx) => (
+//           <ListGroup.Item key={idx}>{contact}</ListGroup.Item>
+//         ))}
+//       </ListGroup>
+//       <h2>Members</h2>
+//     </>
+//   );
+// }
 
-export default Sidebar;
+// export default Sidebar;
--- client/src/components/pages/Messages/Messages.js
+++ client/src/components/pages/Messages/Messages.js
@@ -1,15 +1,15 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import Sidebar from "../../Sidebar";
+// import Sidebar from "../../Sidebar";
 import MessageForm from "../../MessageForm";
 
 function Messages() {
   return (
     <Container>
       <Row>
-        <Col md={4}>
+        {/* <Col md={4}>
           <Sidebar />
-        </Col>
+        </Col> */}
         <Col md={8}>
           <MessageForm />
         </Col>
