82e133054c752717e08096506d7ad31ea669649c
meriem0319 <meriem0319@gmail.com>
2022-11-11 09:18:00 -0500

    added authentication for notes page

--- client/src/components/Notes.js
+++ client/src/components/Notes.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "./Notes.css";
 import Draggable from "react-draggable";
 import { v4 as uuidv4 } from "uuid";
+
+import Auth from "../utils/auth";
+
 var randomColor = require("randomcolor");
 
 function Notes() {
@@ -47,36 +51,52 @@ function Notes() {
   };
 
   return (
-    <div className="NoteApp">
-      <div className="new-item">
-        <input
-          value={item}
-          onChange={(e) => setItem(e.target.value)}
-          placeholder="Enter Note..."
-          onKeyPress={(e) => keyPress(e)}
-        />
-        <button onClick={newItem}>SAVE NOTE</button>
-      </div>
-      <div id="items">
-        {items.map((item, index) => {
-          return (
-            <Draggable
-              key={item.id}
-              defaultPosition={item.defaultPos}
-              onStop={(e, data) => {
-                updatePos(data, index);
-              }}
-            >
-              <div style={{ backgroundColor: item.color }} className="box">
-                <p style={{ margin: 0 }}>{item.item}</p>
-                <button id="delete" onClick={(e) => deleteNote(item.id)}>
-                  X
-                </button>
-              </div>
-            </Draggable>
-          );
-        })}
-      </div>
+    <div>
+      <h3>Create a Note</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+          <div className="NoteApp">
+            <div className="new-item">
+              <input
+                value={item}
+                onChange={(e) => setItem(e.target.value)}
+                placeholder="Enter Note..."
+                onKeyPress={(e) => keyPress(e)}
+              />
+              <button onClick={newItem}>SAVE NOTE</button>
+            </div>
+            <div id="items">
+              {items.map((item, index) => {
+                return (
+                  <Draggable
+                    key={item.id}
+                    defaultPosition={item.defaultPos}
+                    onStop={(e, data) => {
+                      updatePos(data, index);
+                    }}
+                  >
+                    <div
+                      style={{ backgroundColor: item.color }}
+                      className="box"
+                    >
+                      <p style={{ margin: 0 }}>{item.item}</p>
+                      <button id="delete" onClick={(e) => deleteNote(item.id)}>
+                        X
+                      </button>
+                    </div>
+                  </Draggable>
+                );
+              })}
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to write and save notes. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
     </div>
   );
 }
