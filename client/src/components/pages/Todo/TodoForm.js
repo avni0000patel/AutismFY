5c3417e710d08f33e6c2a00697454ae809017e33
meriem0319 <meriem0319@gmail.com>
2022-11-11 09:32:35 -0500

    added authentication for todo page

--- client/src/components/pages/Todo/TodoForm.js
+++ client/src/components/pages/Todo/TodoForm.js
@@ -1,52 +1,72 @@
-import React, {useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+import Auth from "../utils/auth";
 
 function TodoForm(props) {
-    const [input, setInput] = useState(props.edit ? props.edit.value : '');
-    const inputRef = useRef(null);
+  const [input, setInput] = useState(props.edit ? props.edit.value : "");
+  const inputRef = useRef(null);
 
-    useEffect( () => {
-      inputRef.current.focus()
-    })
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
-    const handleChange = e => {
-        setInput(e.target.value)
-    };
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-    const handleSubmit = e => {
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-        props.onSubmit({
-          id:Math.floor(Math.random() * 10000),
-          text: input
-        });
-        setInput('');
-    }
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+    });
+    setInput("");
+  };
   return (
-    <form className="todo-form" onSubmit={handleSubmit}>
-      {props.edit ? ( 
+    <div>
+      <h3>Want to create a To-Do List for Today?</h3>
+
+      {Auth.loggedIn() ? (
         <>
-      <input
-        type="text"
-        placeholder='Update your item'
-        value={input}
-        name="text"
-        className='todo-input'
-        onChange={handleChange}
-        ref={inputRef} 
-        />
-        <button className='todo-button'>Update</button></>) :  
-        (
-          <>
-        <input
-          type="text"
-          placeholder='Add a todo'
-          value={input}
-          name="text"
-          className='todo-input'
-          onChange={handleChange}
-          ref={inputRef} /><button className='todo-button'>Add todo</button></>)
-        }
-    </form>
+          <form className="todo-form" onSubmit={handleSubmit}>
+            {props.edit ? (
+              <>
+                <input
+                  type="text"
+                  placeholder="Update your item"
+                  value={input}
+                  name="text"
+                  className="todo-input"
+                  onChange={handleChange}
+                  ref={inputRef}
+                />
+                <button className="todo-button">Update</button>
+              </>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  placeholder="Add a todo"
+                  value={input}
+                  name="text"
+                  className="todo-input"
+                  onChange={handleChange}
+                  ref={inputRef}
+                />
+                <button className="todo-button">Add todo</button>
+              </>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to create a To-Do List. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
   );
 }
 
