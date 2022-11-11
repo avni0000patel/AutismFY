fb2f90a8c09c509d331256f369a0514146eec2f2
Avni <avni0000patel@gmail.com>
2022-11-11 17:50:34 -0500

    add a todo  centered

--- client/src/components/pages/Todo/TodoForm.js
+++ client/src/components/pages/Todo/TodoForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
+  const styles = {
+    add__todo__button: {
+      color: 'white',
+      background: 'linear-gradient(90deg, rgba(93, 12, 255, 1) 0%, rgba(155, 0, 250, 1) 100%) ',
+    }
+  }
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
   const inputRef = useRef(null);
 
@@ -22,35 +28,50 @@ function TodoForm(props) {
     setInput("");
   };
   return (
-    <form className="todo-form" onSubmit={handleSubmit}>
+    <form className="todo-form flex-row justify-center justify-space-between-md align-center" onSubmit={handleSubmit}>
       {props.edit ? (
         <>
-          <input
-            type="text"
-            placeholder="Update your item"
-            value={input}
-            name="text"
-            className="todo-input"
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <button className="todo-button">Update</button>
+          <div className="col-12 col-lg-9">
+            <input
+              type="text"
+              placeholder="Update your item"
+              value={input}
+              name="text"
+              className="form-input w-100"
+              style={{ lineHeight: '1.5', resize: 'vertical', border: '2px solid #5d0cff', }}
+              onChange={handleChange}
+              ref={inputRef}
+            />
+          </div>
+          <div className="col-12 col-lg-3">
+            <button className="add__todo__button btn btn-block py-3" style={styles.add__todo__button} type="submit">
+              Update Todo
+            </button>
+          </div>
         </>
       ) : (
         <>
-          <input
-            type="text"
-            placeholder="Add a todo"
-            value={input}
-            name="text"
-            className="todo-input"
-            onChange={handleChange}
-            ref={inputRef}
-          />
-          <button className="todo-button">Add todo</button>
+          <div className="col-12 col-lg-9">
+            <input
+              type="text"
+              placeholder="Add a todo"
+              value={input}
+              name="text"
+              className="form-input w-100"
+              style={{ lineHeight: '1.5', resize: 'vertical', border: '2px solid #5d0cff', }}
+              onChange={handleChange}
+              ref={inputRef}
+            />
+          </div>
+          <div className="col-12 col-lg-3">
+            <button className="add__todo__button btn btn-block py-3" style={styles.add__todo__button} type="submit">
+              Add Todo
+            </button>
+          </div>
         </>
-      )}
-    </form>
+      )
+      }
+    </form >
   );
 }
 
