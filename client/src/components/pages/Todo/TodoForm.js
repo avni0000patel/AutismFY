0a41879076a19cf5774f63bd870741f77c154a5d
meriem0319 <meriem0319@gmail.com>
2022-11-11 09:45:09 -0500

    added auth dotolist, fixed notes, footer todo name

--- client/src/components/pages/Todo/TodoForm.js
+++ client/src/components/pages/Todo/TodoForm.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-
-import Auth from "../utils/auth";
 
 function TodoForm(props) {
   const [input, setInput] = useState(props.edit ? props.edit.value : "");
@@ -25,48 +22,35 @@ function TodoForm(props) {
     setInput("");
   };
   return (
-    <div>
-      <h3>Want to create a To-Do List for Today?</h3>
-
-      {Auth.loggedIn() ? (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      {props.edit ? (
         <>
-          <form className="todo-form" onSubmit={handleSubmit}>
-            {props.edit ? (
-              <>
-                <input
-                  type="text"
-                  placeholder="Update your item"
-                  value={input}
-                  name="text"
-                  className="todo-input"
-                  onChange={handleChange}
-                  ref={inputRef}
-                />
-                <button className="todo-button">Update</button>
-              </>
-            ) : (
-              <>
-                <input
-                  type="text"
-                  placeholder="Add a todo"
-                  value={input}
-                  name="text"
-                  className="todo-input"
-                  onChange={handleChange}
-                  ref={inputRef}
-                />
-                <button className="todo-button">Add todo</button>
-              </>
-            )}
-          </form>
+          <input
+            type="text"
+            placeholder="Update your item"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Update</button>
         </>
       ) : (
-        <p>
-          You need to be logged in to create a To-Do List. Please{" "}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
-        </p>
+        <>
+          <input
+            type="text"
+            placeholder="Add a todo"
+            value={input}
+            name="text"
+            className="todo-input"
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Add todo</button>
+        </>
       )}
-    </div>
+    </form>
   );
 }
 
--- client/src/components/pages/Todo/TodoList.js
+++ client/src/components/pages/Todo/TodoList.js
@@ -1,50 +1,69 @@
-import React, {useState} from 'react';
-import TodoForm from './TodoForm'
-import Todo from './Todo';
-import './Todo.css';
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+import Auth from "../../../utils/auth";
+
+import TodoForm from "./TodoForm";
+import Todo from "./Todo";
+import "./Todo.css";
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = todo =>{
-        if(!todo.text || /^\s*$/.test(todo.text)){
-            return
-        }
-
-        const newTodos = [todo, ...todos]
-
-        setTodos(newTodos);
-    };
-
-    const updateTodo = (todoId, newValue) =>{
-      if(!newValue.text || /^\s*$/.test(newValue.text)){
-        return
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
-    setTodos(prev => prev.map(item => (item.id===todoId ? newValue : item))
-    );
 
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
   };
 
-    const removeTodo = id =>{
-      const removeArr = [...todos].filter(todo => todo.id !== id)
-      setTodos(removeArr);
-    };
-
-    const completeTodo = id =>{
-      let updatedTodos = todos.map(todo =>{
-        if (todo.id === id) {
-          todo.isComplete = !todo.isComplete
-        }
-        return todo
-      })
-      setTodos(updatedTodos);
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
     }
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
   return (
     <div>
-      <h1>What is the plan for today?</h1>
-      <TodoForm onSubmit={addTodo} />
-      <Todo todos={todos} completeTodo={completeTodo} 
-      removeTodo={removeTodo} updateTodo={updateTodo}/>
+      {Auth.loggedIn() ? (
+        <>
+          <div>
+            <h1>What is the plan for today?</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+              todos={todos}
+              completeTodo={completeTodo}
+              removeTodo={removeTodo}
+              updateTodo={updateTodo}
+            />
+          </div>
+        </>
+      ) : (
+        <p>
+          You need to be logged in to write a To-Do list. Please{" "}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
     </div>
   );
 }
