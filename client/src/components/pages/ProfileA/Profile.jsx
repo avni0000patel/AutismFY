2012094135a4fa4432abfa8d0ccb5d205d9e7508
OLESIA CHUGAIEVA <ALESYAPRITCHARD@GMAIL.COM>
2022-11-11 20:08:44 -0500

    newStyleToProfile

--- client/src/components/pages/ProfileA/Profile.jsx
+++ client/src/components/pages/ProfileA/Profile.jsx
@@ -1,16 +1,14 @@
 import style from './Profile.module.css'
 
-
 import UploadAvatar from './UploadAvatar/UploadAvatar'
 import BioText from './BioBlock/BioText'
 import BioChanger from './BioBlock/BioChange'
-import ProfileCards from './ProfileCards/ProfileCards'
 
 const Profile = (props) => {
     return(
         <>
-        <div className="flex-row justify-center">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-1">Viewing your Profile</h2>
+        <div className={style.welcome}>
+        <h2 className= {style.welcome}>Your AutismFY Profile</h2>
         </div>
             <div className={style.profileBlock}>
             {!props.profile.changer? <img className={style.imgProfile} src={`http://localhost:3001/static/img/${props.profile.imgSrc}`} alt=""/> : <UploadAvatar setProfile={props.setProfile} reloadWithImg={props.changeFormBio} user={props.user}/>}
@@ -33,7 +31,6 @@ const Profile = (props) => {
                     }
                 </div>
             </div>
-            <ProfileCards/>
     </>
     )
 }
